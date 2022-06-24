666e529d346f5624d9098d6521864659a44b8064
Taehong Kim <lkoiescg2031@naver.com>
2022-06-25 02:33:16 +0900

    fix : BlogLayoutDrawer @emotion 라이브러리 적용
    #1

--- src/layout/BlogLayout/BlogLayoutDrawer.tsx
+++ src/layout/BlogLayout/BlogLayoutDrawer.tsx
@@ -21,7 +21,6 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 import BlogLayoutContext from "./BlogLayoutContext";
 import { ClassNames } from "@emotion/react";
-import { Toolbar } from "@mui/material";
 
 // import PostsButton from "./PostsButton";
 // import Tags from "../../components/Tags";
@@ -29,42 +28,7 @@ import { Toolbar } from "@mui/material";
 export const drawerWidth = 280;
 
 // const useStyles = makeStyles((theme: Theme) => ({
-// 	drawer: {},
-// 	drawerPaper: {
-// 	},
-// 	transParentBackground: {
-// 		backgroundColor: "#00000000",
-// 	},
-// 	toolbar: {
-// 	},
-// 	closeButton: {
-// 		position: "absolute",
-// 		right: "3px",
-// 	},
-// 	profileRoot: {
-// 		display: "flex",
-// 		flexFlow: "column nowrap",
-// 		alignItems: "center",
-// 		paddingTop: theme.spacing(2),
-// 		paddingBottom: theme.spacing(2),
-// 	},
-// 	avatar: {
-// 		width: theme.spacing(12),
-// 		height: theme.spacing(12),
-
-// 		border: `4px solid ${theme.palette.primary.dark}`,
-// 		backgroundColor: "#fff",
-
-// 		color: theme.palette.primary.dark,
-// 		fontSize: theme.spacing(6),
-// 	},
-// 	contactWrapper: {
-// 		display: "flex",
-// 		marginTop: theme.spacing(1),
-// 	},
 // 	contactIcon: {
-// 		width: theme.spacing(4),
-// 		height: theme.spacing(4),
 // 	},
 // 	postsTreeRoot: {
 // 		height: 240,
@@ -82,16 +46,17 @@ const StyledSwipeableDrawer = styled((props: SwipeableDrawerProps) => (
 				classes={{
 					paper: css`
 						width: ${drawerWidth}px;
-						overflow: "overlay;
+						overflow: overlay;
 					`,
 				}}
 			/>
 		)}
 	</ClassNames>
-))``;
+))`
+	z-index: 1300;
+`;
 
-// 		...theme.mixins.toolbar,
-const StyledToolbar = styled((props: { children: React.ReactElement }) => (
+const StyledToolbar = styled((props: { children: JSX.Element }) => (
 	<div {...props} />
 ))(({ theme }) => ({
 	...theme.mixins.toolbar,
@@ -99,6 +64,47 @@ const StyledToolbar = styled((props: { children: React.ReactElement }) => (
 	alignItems: "center",
 	position: "relative",
 }));
+
+const StyledCloseIconButton = styled(IconButton)(({ theme }) => ({
+	position: "absolute",
+	right: "3px",
+}));
+
+const StyledProfileRoot = styled(
+	(props: { children: React.ReactElement[] }) => <div {...props} />
+)(({ theme }) => ({
+	display: "flex",
+	flexFlow: "column nowrap",
+	alignItems: "center",
+	paddingTop: theme.spacing(2),
+	paddingBottom: theme.spacing(2),
+}));
+
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+	width: theme.spacing(12),
+	height: theme.spacing(12),
+
+	border: `4px solid ${theme.palette.primary.dark}`,
+	backgroundColor: "#fff",
+
+	color: theme.palette.primary.dark,
+	fontSize: theme.spacing(6),
+}));
+
+const StyledContactWrapper = styled(
+	(props: { children: ("" | undefined | React.ReactElement)[] }) => (
+		<div {...props} />
+	)
+)(({ theme }) => ({
+	display: "flex",
+	marginTop: theme.spacing(1),
+}));
+
+const StyledSNSIconButton = styled(IconButton)(({ theme }) => ({
+	width: theme.spacing(4),
+	height: theme.spacing(4),
+}));
+
 const BlogLayoutDrawer: React.FC = () => {
 	const { isOpenDrawer, toggleDrawer, profile, categories, tags } =
 		useContext(BlogLayoutContext);
@@ -112,85 +118,57 @@ const BlogLayoutDrawer: React.FC = () => {
 				onClose={toggleDrawer}
 			>
 				<StyledToolbar>
-					<IconButton
-						// className={classes.closeButton}
-						onClick={toggleDrawer}
-					>
+					<StyledCloseIconButton onClick={toggleDrawer}>
 						<ArrowBackIosIcon />
-					</IconButton>
+					</StyledCloseIconButton>
 				</StyledToolbar>
 				<Divider />
 				{profile && (
 					<>
-						<div
-						// className={classes.profileRoot}
-						>
-							<Avatar
-								alt={profile.name}
-								src={profile.figure}
-								// className={classes.avatar}
-							>
+						<StyledProfileRoot>
+							<StyledAvatar alt={profile.name} src={profile.figure}>
 								{typeof profile.figure === "undefined" ? profile.name : null}
-							</Avatar>
+							</StyledAvatar>
 							<Typography variant="h6" color="inherit">
 								{profile.name}
 							</Typography>
+							{/** FIXME 상세 정보 마진 설정 */}
 							<Typography variant="body2" color="inherit">
 								{profile.desc}
 							</Typography>
-							<div
-							// className={classes.contactWrapper}
-							>
+							<StyledContactWrapper>
 								{profile.email && (
-									<IconButton
-										// className={classes.contactIcon}
-										href={`mailto:${profile.email}`}
-									>
+									<StyledSNSIconButton href={`mailto:${profile.email}`}>
 										<EmailIcon />
-									</IconButton>
+									</StyledSNSIconButton>
 								)}
 								{profile.github && (
-									<IconButton
-										// className={classes.contactIcon}
-										href={profile.github}
-									>
+									<StyledSNSIconButton href={profile.github}>
 										<GithubIcon />
-									</IconButton>
+									</StyledSNSIconButton>
 								)}
 								{profile.facebook && (
-									<IconButton
-										// className={classes.contactIcon}
-										href={profile.facebook}
-									>
+									<StyledSNSIconButton href={profile.facebook}>
 										<FacebookIcon />
-									</IconButton>
+									</StyledSNSIconButton>
 								)}
 								{profile.twitter && (
-									<IconButton
-										// className={classes.contactIcon}
-										href={profile.twitter}
-									>
+									<StyledSNSIconButton href={profile.twitter}>
 										<TwitterIcon />
-									</IconButton>
+									</StyledSNSIconButton>
 								)}
 								{profile.instagram && (
-									<IconButton
-										// className={classes.contactIcon}
-										href={profile.instagram}
-									>
+									<StyledSNSIconButton href={profile.instagram}>
 										<InstagramIcon />
-									</IconButton>
+									</StyledSNSIconButton>
 								)}
 								{profile.linkedIn && (
-									<IconButton
-										// className={classes.contactIcon}
-										href={profile.linkedIn}
-									>
+									<StyledSNSIconButton href={profile.linkedIn}>
 										<LinkedInIcon />
-									</IconButton>
+									</StyledSNSIconButton>
 								)}
-							</div>
-						</div>
+							</StyledContactWrapper>
+						</StyledProfileRoot>
 						<Divider />
 						{/* TODO 카테고리 노출 기능 */}
 						{/* {(function renderPosts(root, level = 0) {
