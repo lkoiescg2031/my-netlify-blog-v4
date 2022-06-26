26a364f902366bcaac0938fbb342b14a64f25c83
Taehong Kim <lkoiescg2031@naver.com>
2022-06-26 19:56:34 +0900

    fix : canvas.ts 타입 오류 수정
    #1

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -4,7 +4,10 @@ export const createGradient =
 		const { height } = ctx.canvas;
 		const canvasGradient = ctx.createLinearGradient(0, 0, 0, height);
 
-		Object.entries(gradientOptions).forEach(([offset, color]) => {
+		Object.entries(gradientOptions).forEach(([key, value]) => {
+			const offset = key as unknown as number;
+			const color = value as string;
+
 			canvasGradient.addColorStop(offset, color);
 		});
 
