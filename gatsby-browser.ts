7f566a2ef19f20c8dbb4b0817c86bd719d6617a1
Taehong Kim <lkoiescg2031@naver.com>
2022-07-03 15:04:14 +0900

    feature: global style 추가
    #1

---
+++ gatsby-browser.ts
@@ -0,0 +1,25 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+
+// icon fonts
+import "./src/fonts/XEIcon-2.2.0/xeicon.min.css";
+import "./src/fonts/fontawesome-5.15.2/css/all.min.css";
+
+// fonts
+// FIXME 폰트 업데이트 확인하기
+// import 'fontsource-roboto';
+import "@fontsource/roboto";
+
+// markdown syntax highlight
+// FIXME markdown syntax 하이라이트 css 추가
+// import "prismjs/themes/prism-coy.css";
+// import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+// import "prismjs/plugins/command-line/prism-command-line.css";
+
+// global styles
+import "./src/styles/global.css";
