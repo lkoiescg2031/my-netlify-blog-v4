fcaa4b0f46e86be8bacaa4b9ca35867169853f32
Taehong Kim <lkoiescg2031@naver.com>
2022-07-07 22:08:48 +0900

    feature : 404 페이지 이전
    #1

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,54 +1,19 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import WarningIcon from "@mui/icons-material/WarningRounded";
+import BreakoutGame from "@lkoiescg2031/react-breakout";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import BlogLayout from "../layout/BlogLayout";
+import SEO from "../components/SEO";
 
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+const NotFoundPage = () => (
+	<BlogLayout>
+		<SEO title="404: Not found" />
+		<WarningIcon style={{ fontSize: "75px" }} color="error" />
+		<h1>404: 페이지를 찾을 수 없음</h1>
+		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<BreakoutGame />
+	</BlogLayout>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
