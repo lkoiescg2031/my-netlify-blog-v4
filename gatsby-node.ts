c12c9bdd94487c9cec4a470ebc1590bb443eee5a
Taehong Kim <lkoiescg2031@naver.com>
2022-07-07 22:00:17 +0900

    fix : gatsby-node 오타 수정
    #1

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,7 +2,7 @@ import { GatsbyNode } from "gatsby";
 
 import MdxPageNodeAdapter from "./gatsby-node/MdxPageNodeAdapter";
 
-const mdxPageNodeAdapter = new MdxPageNodeAdapter( "./src/templates/Post.tsx")ß
+const mdxPageNodeAdapter = new MdxPageNodeAdapter("./src/templates/Post.tsx");
 
 const gatsbyNode: GatsbyNode = {
 	async createPages(args) {
